210e8fb648e1b4e7a044ffa405226471c2e56b43
Asanilta Fahda <asanilta@gmail.com>
2015-10-14 16:28:44 +0700

    Checksum

---
+++ checksum.c
@@ -0,0 +1,18 @@
+unsigned short checksum(Byte *data, size_t bytes )
+{
+        unsigned short sum1 = 0xff, sum2 = 0xff;
+ 
+        while (bytes) {
+                size_t tlen = bytes > 20 ? 20 : bytes;
+                bytes -= tlen;
+                do {
+                        sum2 += sum1 += *data++;
+                } while (--tlen);
+                sum1 = (sum1 & 0xff) + (sum1 >> 8);
+                sum2 = (sum2 & 0xff) + (sum2 >> 8);
+        }
+        /* Second reduction step to reduce sums to 8 bits */
+        sum1 = (sum1 & 0xff) + (sum1 >> 8);
+        sum2 = (sum2 & 0xff) + (sum2 >> 8);
+        return sum2 << 8 | sum1;
+}
